a394e0b0072413a964e8f381025d8dbc17ef9387
YulianSalo <julian_salo@yahoo.com>
2019-12-11 22:14:40 +0200

    Heroku

--- django_web_app/django_web_app/settings.py
+++ django_web_app/django_web_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import psycopg2
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,3 +134,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
